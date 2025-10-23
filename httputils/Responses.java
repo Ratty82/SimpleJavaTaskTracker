4f49185c4f520419c11d25c2d042d46d34da639f
Ratty82 <rat911@yandex.ru>
2025-10-23 16:44:10 +0300

    Refactored Responses

--- httputils/Responses.java
+++ httputils/Responses.java
@@ -1,8 +1,6 @@
 package httputils;
-
 import com.google.gson.Gson;
 import com.sun.net.httpserver.HttpExchange;
-
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
@@ -13,7 +11,7 @@ public class Responses {
 
     public static void sendError(HttpExchange hex, Integer code, String body) throws IOException {
         byte[] bytesToSend = body.getBytes(StandardCharsets.UTF_8);
-        hex.getResponseHeaders().set("Content-Type", "Application/JSON");
+        hex.getResponseHeaders().set("Content-Type", "text/plain");
         hex.sendResponseHeaders(code,bytesToSend.length);
         OutputStream os = hex.getResponseBody();
         os.write(bytesToSend);
